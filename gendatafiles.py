353bb662eb4dcce4187de3bfb8cb2dca1e1605b3
Britodfbr <britodfbr@gmail.com>
2021-02-09 09:47:49 -0300

    tratativa para gerar VENDAS aleatórias e VENDEDORES fixo

--- gendatafiles.py
+++ gendatafiles.py
@@ -4,14 +4,28 @@ __author__ = '@britodfbr'
 import pandas as pd
 from faker import Faker
 import locale
+import random
+import toml
+from pathlib import Path
 
+dados = Path('dados/dados.toml')
+dados.parent.mkdir(exist_ok=True)
 locale.setlocale(locale.LC_TIME, 'pt_BR.utf8')
+fake = Faker('pt_BR')
+
+
+def gen_names():
+    fake.seed_instance(0)
+    names = {'VENDEDOR': (f"{fake.first_name()} {fake.last_name()}" for x in range(1000))}
+    with open('dados/dados.toml', 'w') as f:
+        toml.dump(names, f)
 
 
 def gendata():
-    fake = Faker('pt_BR')
-    fake.seed_instance(0)
-    names = (f"{fake.first_name()} {fake.last_name()}" for x in range(1000))
+    if not dados.is_file():
+        gen_names()
+    names = toml.load(dados)['VENDEDOR']
+    fake.seed_instance(random.randint(1, 13))
     vendas = (
         fake.pyfloat(left_digits=None, right_digits=2, positive=True, min_value=1000, max_value=55000)
         for _ in range(1000)
@@ -21,9 +35,11 @@ def gendata():
 
 def run():
     meses = set(pd.date_range(start='2020-01-01', end='2020-6-1', periods=30).strftime('%B'))
+    # print(list(gendata()))
     df = pd.DataFrame(gendata(), columns=['VENDEDOR', 'VENDAS'])
     print(len(meses), meses)
     print(df)
+    print(random.randint(1, 13))
     # for i in range(10):
     #     print(f"{fake.first_name()} {fake.last_name()}",
     #           fake.pyfloat(left_digits=None, right_digits=2, positive=True, min_value=10000, max_value=60000)
