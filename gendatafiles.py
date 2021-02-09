caf6a951f17402dc18d5b28facf4a541c04d4441
Britodfbr <britodfbr@gmail.com>
2021-02-09 10:12:35 -0300

    Sanitizado e documentado

--- gendatafiles.py
+++ gendatafiles.py
@@ -15,31 +15,41 @@ fake = Faker('pt_BR')
 
 
 def gen_names():
+    """ Gera os 1000 nomes aleatórios utilizando a biblioteca Faker """
+    # Semente para garantir que sempre serão os mesmos nomes
     fake.seed_instance(0)
+    # dict com os 1000 nomes completos
     names = {'VENDEDOR': (f"{fake.first_name()} {fake.last_name()}" for x in range(1000))}
+    # grava o resultado em um arquivo toml para recuperação posterior
     with open('dados/dados.toml', 'w') as f:
         toml.dump(names, f)
 
 
 def gendata():
+    """ Gera massa de dados com VENDEDOR/VENDAS """
+    # Se não existir cria arquivo toml com nomes de entrada
     if not dados.is_file():
         gen_names()
+    # Carrega nomes dos vendedores
     names = toml.load(dados)['VENDEDOR']
+    # Semente aleatória para gerar valor de vendas diferentes
     fake.seed_instance(random.randint(1, 13))
+    # gerador com 1000 entradas aleatórias para vendas
     vendas = (
-        fake.pyfloat(left_digits=None, right_digits=2, positive=True, min_value=1000, max_value=55000)
+        fake.pyfloat(left_digits=None, right_digits=2, positive=True, min_value=1000, max_value=55100)
         for _ in range(1000)
     )
+    # Retorna uma lista de tuplas com VENDEDOR/VENDA
     return zip(names, vendas)
 
 
 def run():
+    """"""
+    # Lista com nomes dos meses gerado em pandas
     meses = set(pd.date_range(start='2020-01-01', end='2020-6-1', periods=30).strftime('%B'))
-    # print(list(gendata()))
+    # DataFrame com conjunto de dados gerados
     df = pd.DataFrame(gendata(), columns=['VENDEDOR', 'VENDAS'])
-    print(len(meses), meses)
-    print(df)
-    print(random.randint(1, 13))
+    # Grava arquivos com nomes dos meses em portugues com vendedores e suas respectivas vendas mensais
     for mes in meses:
         df.to_excel(dados.with_name(f"{mes}.xlsx"), index=False)
 
